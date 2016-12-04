5deffe9fa056bd8de7a3afcfc875af4d43bec8e6
mjsaka <qiuxuenan@gmail.com>
2016-12-04 17:48:08 +0800

    添加学习单元历史记录模型

---
+++ src/model/item_log_model.cpp
@@ -0,0 +1,8 @@
+//
+//  item_log.cpp
+//  ddtype
+//  created by mjsaka on 2016/11/2.
+//  Copyright © 2016 mjsaka. All rights reserved.
+//
+
+#include "item_log.hpp"
---
+++ src/model/item_log_model.hpp
@@ -0,0 +1,10 @@
+//
+//  item_log_model.hpp
+//  ddtype
+//  created by mjsaka on 2016/11/2.
+//  Copyright © 2016 mjsaka. All rights reserved.
+//
+#ifndef item_log_model_hpp
+#define item_log_model_hpp
+
+#endif
