96f1f1e0f3d5c7ee7624d52a78f208409843752b
mjsaka <qiuxuenan@gmail.com>
2016-12-06 23:16:14 +0800

    type_controller和type_interface

--- src/controller/type_controller.cpp
+++ src/controller/type_controller.cpp
@@ -6,3 +6,18 @@
 //
 
 #include "type_controller.hpp"
+#include <sigc++/sigc++.h>
+
+namespace DDType 
+{
+	TypeController::TypeController(TypeInterface& window):m_window(window)
+	{
+		m_window.set_type_callback(sigc::mem_fun(*this, &TypeController::on_typed));
+	}
+	void TypeController::set_unit_path(Glib::ustring)
+	{
+	}
+	void TypeController::on_typed(char c)
+	{
+	}
+}
--- src/controller/type_controller.hpp
+++ src/controller/type_controller.hpp
@@ -4,3 +4,25 @@
 //  created by mjsaka on 2016/11/2.
 //  Copyright © 2016 mjsaka. All rights reserved.
 //
+#ifndef type_controller_hpp
+#define type_controller_hpp
+#include <glibmm.h>
+#include "../view/type_interface.hpp"
+#include "../model/item_data_model.hpp"
+
+namespace DDType
+{
+	class TypeController
+	{
+		public:
+			TypeController(TypeInterface& window);
+			void on_typed(char c);
+			void set_unit_path(Glib::ustring path);
+		private:
+			TypeInterface& m_window;
+			std::list<ItemData> m_item_data_list;
+			int m_max_code_len_of_current_item;
+			std::vector<char> m_input;
+	};
+}
+#endif
